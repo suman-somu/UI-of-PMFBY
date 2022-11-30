f28cc11a3d71c39d0ce8483594997acef72263f3
Suman Sahoo <ssamsomu@gmail.com>
2022-12-01 02:10:20 +0530

    firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:pbyj_app/Homepage/FarmerHomePage.dart';
 import 'package:pbyj_app/Homepage/MainHome.dart';
 import 'package:pbyj_app/utils/routes.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'pages/home.dart';
 import 'pages/login.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         MyRoutes.homeRoute: (context) => const HomePage(),
         MyRoutes.loginRoute: (context) => const LoginPage(),
         MyRoutes.FarmerHome: (context) => const FarmerHome(),
-        MyRoutes.MainHome:(context) => const MainHome(),
+        MyRoutes.MainHome: (context) => const MainHome(),
         // MyRoutes.cartRoute: (context) => CartPage(),
       },
     );
